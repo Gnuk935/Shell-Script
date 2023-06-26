266e35a5c8d2315a2847ad6cbd381fa08cfd326c
Victor Barros <89825674+Gnuk935@users.noreply.github.com>
2023-06-25 23:28:32 -0400

    vapo

---
+++ inst_flat.sh
@@ -0,0 +1,5 @@
+ echo "===================================="
+ echo "Instalando oo flatpak"
+ echo "===================================="
+ sudo apt install -y flatpak
+ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
