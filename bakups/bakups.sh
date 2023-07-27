324959210eddb2016f825eff9c0014ad99906054
root <root@pop-os.localdomain>
2023-07-27 04:18:30 -0400

    bugando sabosta

--- bakups/bakups.sh
+++ bakups/bakups.sh
@@ -4,12 +4,15 @@ dia=$(date +%d)
 mes=$(date +%m)
 ano=$(date +%Y)
 nomeArquivo=$($ano\-$mes\-$dia)
+$nomeArquivo
+mkdir /bakups/zipados/$nomeArquivo
 echo "Criando array com os nomes de arquivos zip"
 listNameZips=("teste1.zip" "teste2.zip" "teste3.zip")
 echo "Array gerado:\ ${listNameZips[@]}"
 echo "Criando array com os nomes das pastas"
 listNamePastas=("teste1" "teste2" "teste3")
 echo "Array gerado:\ ${listNamePastas[@]}"
+touch /bakups/hahs/$nomeArquivo
 for i in "${!listNamePastas[@]}"
 do
 	echo "Zipando\ ${listNamePastas[$i]}"
@@ -17,7 +20,8 @@ do
 	pastaName=${listNamePastas[$i]}
         echo $zipName
 	echo $pastaName
-	zip -r9 $zipName $pastaName | sha256sum >> hashs
+	zip -r9 /bakups/aZipar/$nomeArquivo/$zipName $pastaName | sha256sum >> /bakups/hahs/$nomeArquivo
         echo "Verificando integridade"
-        zip -T $zipName
+        zip -T /bakups/aZipar/$nomeArquivo/$zipName
 done
+zip -r9 $nomeArquivo\.zip /bakups/zipados/$nomeArquivo/ | sha256sum >> /bakups/hahs/$nomeArquivo
