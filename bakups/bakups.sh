63f84836abc2463931e2bc67b4e48f74087047c8
Victor Barros <89825674+Gnuk935@users.noreply.github.com>
2023-07-27 13:08:46 -0400

    feat: finaliza script

--- bakups/bakups.sh
+++ bakups/bakups.sh
@@ -3,25 +3,33 @@ echo "Iniciando processo de backups"
 dia=$(date +%d)
 mes=$(date +%m)
 ano=$(date +%Y)
-nomeArquivo=$($ano\-$mes\-$dia)
-$nomeArquivo
-mkdir /bakups/zipados/$nomeArquivo
+nomeArquivo=$(echo $ano\-$mes\-$dia)
+echo "O nomo do arquivo é:\ $nomeArquivo"
+mkdir /backups/aZipar/$nomeArquivo
 echo "Criando array com os nomes de arquivos zip"
 listNameZips=("teste1.zip" "teste2.zip" "teste3.zip")
 echo "Array gerado:\ ${listNameZips[@]}"
+echo "====================================="
 echo "Criando array com os nomes das pastas"
 listNamePastas=("teste1" "teste2" "teste3")
 echo "Array gerado:\ ${listNamePastas[@]}"
-touch /bakups/hahs/$nomeArquivo
+touch /backups/hahs/$nomeArquivo
 for i in "${!listNamePastas[@]}"
 do
+	echo "=============================="
 	echo "Zipando\ ${listNamePastas[$i]}"
 	zipName=${listNameZips[$i]}
 	pastaName=${listNamePastas[$i]}
         echo $zipName
 	echo $pastaName
-	zip -r9 /bakups/aZipar/$nomeArquivo/$zipName $pastaName | sha256sum >> /bakups/hahs/$nomeArquivo
+	echo "============================="
+	zip -r9 /backups/aZipar/$nomeArquivo/$zipName $pastaName | sha256sum >> /backups/hahs/$zipName\.sha256
         echo "Verificando integridade"
-        zip -T /bakups/aZipar/$nomeArquivo/$zipName
+        zip -T /backups/aZipar/$nomeArquivo/$zipName
 done
-zip -r9 $nomeArquivo\.zip /bakups/zipados/$nomeArquivo/ | sha256sum >> /bakups/hahs/$nomeArquivo
+echo "================================================="
+echo "Zipando pasta principal"
+zip -r9 /backups/zipados/$nomeArquivo\.zip /backups/aZipar/$nomeArquivo/ | sha256sum >> /backups/hahs/$nomeArquivo\.sha256
+echo "Verificando integridade da pasta"
+zip -T /backups/zipados/$nomeArquivo\.zip
+rm -r /backups/aZipar/$nomeArquivo/
