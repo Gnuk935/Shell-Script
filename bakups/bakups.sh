fe4b299c5db505c76c1ad8f17a450de2ef9c21f9
Gnuk935 <victorbmc9@gmail.com>
2023-09-03 15:37:23 -0400

    awas

--- bakups/bakups.sh
+++ bakups/bakups.sh
@@ -1,35 +1,46 @@
-#!bin/bash
+#!/usr/bin/bash
 echo "Iniciando processo de backups"
-dia=$(date +%d)
-mes=$(date +%m)
-ano=$(date +%Y)
-nomeArquivo=$(echo $ano\-$mes\-$dia)
-echo "O nomo do arquivo é:\ $nomeArquivo"
-mkdir /backups/aZipar/$nomeArquivo
+USER=$(whoami)
+HOME=/home/$USER
+DIA=$(date +%d)
+MES=$(date +%m)
+ANO=$(date +%Y)
+LOGS=/var/log/teste
+NOMEARQUIVO=$(echo $ANO\-$MES\-$DIA)
+LOGSDIA=$LOGS/$NOMEARQUIVO
+AZIP=/teste/testeAzip
+DIRHASH=/teste/hashs
+DIRZIPADOS=/teste/zipados
+echo "O nome do log é:\ $LOGSDIA"
+echo "O nome do arquivo é:\ $NOMEARQUIVO"
+mkdir -v $AZIP/$NOMEARQUIVO
 echo "Criando array com os nomes de arquivos zip"
-listNameZips=("teste1.zip" "teste2.zip" "teste3.zip")
-echo "Array gerado:\ ${listNameZips[@]}"
+LISTNAMEZIPS=("test1.zip" "test2.zip" "test3.zip")
+echo "Array gerado:\ ${LISTNAMEZIPS[@]}"
 echo "====================================="
 echo "Criando array com os nomes das pastas"
-listNamePastas=("teste1" "teste2" "teste3")
-echo "Array gerado:\ ${listNamePastas[@]}"
-touch /backups/hahs/$nomeArquivo
-for i in "${!listNamePastas[@]}"
+LISTNAMEPASTAS=("test1" "test2" "test3")
+echo "Array gerado:\ ${LISTNAMEPASTAS[@]}"
+mkdir -v /backups/hahs/$NOMEARQUIVO
+mkdir -v "$LOGSDIA"
+for i in "${!LISTNAMEPASTAS[@]}"
 do
 	echo "=============================="
-	echo "Zipando\ ${listNamePastas[$i]}"
-	zipName=${listNameZips[$i]}
-	pastaName=${listNamePastas[$i]}
-        echo $zipName
-	echo $pastaName
+	echo "Zipando\ ${LISTNAMEPASTAS[$i]}"
+	ZIPNAME=${LISTNAMEZIPS[$i]}
+	PASTANAME=${LISTNAMEPASTAS[$i]}
+        echo $ZIPNAME
+	echo $PASTANAME
+	echo $HOME/$PASTANAME
 	echo "============================="
-	zip -r9 /backups/aZipar/$nomeArquivo/$zipName $pastaName | sha256sum >> /backups/hahs/$zipName\.sha256
+	zip -vr9 $AZIP/$NOMEARQUIVO/$ZIPNAME $HOME/$PASTANAME >> $LOGSDIA/$NOMEARQUIVO.log 2>&1 | sha256sum >> $DIRHASH/$NOMEARQUIVO/$ZIPNAME\.sha256
         echo "Verificando integridade"
-        zip -T /backups/aZipar/$nomeArquivo/$zipName
+        zip -T $AZIP/$NOMEARQUIVO/$ZIPNAME
 done
 echo "================================================="
 echo "Zipando pasta principal"
-zip -r9 /backups/zipados/$nomeArquivo\.zip /backups/aZipar/$nomeArquivo/ | sha256sum >> /backups/hahs/$nomeArquivo\.sha256
+zip -vr9 $DIRZIPADOS/$NOMEARQUIVO\.zip $AZIP/$NOMEARQUIVO/ $LOGSDIA/$NOMEARQUIVO.log 2>&1 | sha256sum >> $DIRHASH/$NOMEARQUIVO\.sha256
 echo "Verificando integridade da pasta"
-zip -T /backups/zipados/$nomeArquivo\.zip
-rm -r /backups/aZipar/$nomeArquivo/
+zip -T /backups/zipados/$NOMEARQUIVO\.zip
+#rm -r /backups/aZipar/$NOMEARQUIVO/
+
