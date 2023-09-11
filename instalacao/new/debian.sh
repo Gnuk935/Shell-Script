74c017aedf07ad94497fafbc8c8be84d5930b18c
Gnuk935 <victorbmc9@gmail.com>
2023-09-11 01:02:52 -0400

    fix: instalacao do java

--- instalacao/new/debian.sh
+++ instalacao/new/debian.sh
@@ -55,7 +55,7 @@ echo "COMUM"
 echo $ESPACO
 	chrome(){
 	NAMETMP="chrome.txt"
-	TMPCHROME=$TEMP/$NAMETMP
+	TMPSDKMAN=$TEMP/$NAMETMP
 	ARQUIVO="google-chrome-stable_current_amd64.deb"
 	touch $TMPCHROME
 	echo "Iniciando a instalação do Google Chrome"
@@ -266,6 +266,108 @@ echo $ESPACO
 		fi
 	}
 main
+}
+
+desenvolvedor(){
+echo $ESPACO
+echo "TIPO DE INSTALAÇÃO:"
+echo "Desenvolvedor"
+echo $ESPACO
+	sdkMan(){
+	NAMETMP="sdkman.txt"
+	TMPSDKMAN=$TEMP/$NAMETMP
+	touch $TMPSDKMAN
+	echo "Iniciando a instalação do Sdkman"
+	echo $ESPACO
+	LINK_SDKMAN="https://get.sdkman.io"
+	ls $HOME | grep .sdkman
+	if [ $? -ne 0 ]
+	then
+		echo "Fazendo download do Sdkman"
+		curl -s $LINK_SDKMAN | bash
+		if [ $? -eq 0 ]
+		then
+			echo "SDKMAN foi baixado com sucesso"
+			echo "0" >> $TMPSDKMAN
+		else
+		echo "Erro do baixar o SDKMAN"
+		echo "1" >> $TMPSDKMAN
+		return 1
+		fi
+	else
+	echo "O sdkman já foi baixado"
+	echo "Seguindo para instalação"
+	echo "0" >> $TMPSDKMAN
+	fi
+	
+	STATS_SDKMAN=$(sed -n '1p' $TMPSDKMAN)
+	if [ $STATS_SDKMAN -eq 0 ]
+	then
+		source "$HOME/.sdkman/bin/sdkman-init.sh"
+		if [ $? -eq 0 ]
+		then
+			echo "0" >> $TMPSDKMAN
+			source $HOME/.bashrc
+			sdk version
+			if [ $? -eq - 0 ]
+			then
+			echo "SDKMAN foi instalado com sucesso"
+			echo "0" >> $TMPSDKMAN
+			return 0
+			else
+			echo "Erro ao instalar o SDKMAN (source)"
+			echo "1" >> $TMPSDKMAN
+			return 1
+			fi
+		else
+		echo "Erro ao instalar o SDKMAN (source)"
+		echo "1" >> $TMPSDKMAN
+		return 1
+		fi
+	else
+	echo "Erro ao verificar se o SDKMAN esta baixado!!"
+	echo "ERR: SD11"
+	fi
+	}
+
+	java(){
+	source $HOME/.bashrc
+	echo $ESPACO
+	NAMETMP="java.txt"
+	TMPJAVA=$TEMP/$NAMETMP
+	touch $TMPJAVA
+	echo "Iniciando a instalação do Java"
+	echo $ESPACO
+	sdk version
+	if [ $? -eq 0 ]
+	then
+		echo "0" >> $TMPJAVA
+		echo "O java 12 será instalado"
+		sdk install java 12.0.2.hs-adpt
+		if [ $? -eq 0 ]
+		then
+			source $BASHRC
+			java --version
+			if [ $? -eq 0 ]
+			then
+				echo "O Java foi instalado com sucesso"
+				echo "0" >> $TMPJAVA
+				return 0
+			else
+			echo "O java não foi instalado"
+			echo "1" >> $TMPJAVA
+			fi
+		else
+		echo "Erroo oa instalar o java"
+		echo "1" >> $TMPJAVA
+		return 1
+		fi
+	else
+	echo "Erro ao verificar o sdkman"
+	echo "1" >> $TMPJAVA
+	return 1
+	fi
+	}
 }
 
 recebeValores(){
