a753fb9ceeba616f8323798ba01bd99a75f88402
Victor Barros <89825674+Gnuk935@users.noreply.github.com>
2024-09-06 01:42:09 -0400

    Update game.deb.sh

--- instalações/old/game.deb.sh
+++ instalações/old/game.deb.sh
@@ -1,24 +1,46 @@
-echo "Configurando o vulkan"
-#sudo apt-get install libvulkan-dev vulkan-tools vulkan-validationlayers >> log
-#sudo apt-get install fizmo-sdl2 libsdl2-2.0-0 libsdl2-dev libsdl2-gfx-1.0-0 libsdl2-gfx-dev libsdl2-image-2.0-0 libsdl2-mixer-2.0-0 libsdl2-net-2.0-0 >> log
+#!/bin/bash
 
-echo "Iniciando a configuração de games"
-sudo apt-get install steam -y >> ~/logHoje
- 
-echo "Instalando o Wine"
-udo apt-get install wine winetricks wine-mono wine_gecko vulkan-icd-loader lib32-vulkan-icd-loader vkd3d lib32-vkd3d gvfs -y >> ~/logHoje
+# Variável para obter data e hora atuais
+LOGFILE=~/Downloads/Log$(date +%Y%m%d%H%M%S).log
 
-echo "Iniciando a instalção do Lutris"
-#sudo apt-get install lutris python-magic >> log
+# Função para logar a saída no terminal e no arquivo ao mesmo tempo
+log() {
+    echo "$1" | tee -a "$LOGFILE"
+}
 
-echo "Instalando dependencias adicionais"
-sudo apt-get install gvfs:i386 wine32-preloader:i386 wine64-preloader wine-binfmt gstreamer1.0-libav:i386 gstreamer1.0-plugins-bad:i386 gstreamer1.0-plugins-ugly winetricks -y >> ~/logHoje
+# Início do script
+log "Configurando Debian para Jogos"
 
-echo "Ferramenta para melhoria do OBS"
-#sudo apt-get instalar ffmpeg libnvidia-encode1 libnvidia-fbc1 nvidia-cuda-toolkit >> log
+log "Resolvendo questões de arquitetura"
+sudo dpkg --add-architecture i386 2>&1 | tee -a "$LOGFILE"
+sudo apt update -y 2>&1 | tee -a "$LOGFILE"
+sudo apt upgrade -y 2>&1 | tee -a "$LOGFILE"
+sudo apt install firmware-misc-nonfree -y 2>&1 | tee -a "$LOGFILE"
 
-echo "Dependencias adiconais para wine e codecs"
-sudo apt-get install mingw-w64 flvmeta smpeg-plaympeg lame mjpegtools x265 x264 nvidia-vdpau-driver mpv mpg123 libxvidcore4 fluidsynth -y >> ~/logHoje
+log "Configuração para Intel"
+sudo apt install intel-microcode -y 2>&1 | tee -a "$LOGFILE"
 
-echo "Melhorando compa com btrfs"
-#sudo apt-get install btrfs-progs duperemove >> log
+log "Instalando drivers Vulkan para Intel com gráficos integrados"
+sudo apt install mesa-vulkan-drivers mesa-vulkan-drivers:i386 -y 2>&1 | tee -a "$LOGFILE"
+
+log "Configuração do Wine"
+sudo apt install wine wine64 wine32 -y 2>&1 | tee -a "$LOGFILE"
+
+log "Configurando bibliotecas SDL2"
+sudo apt install fizmo-sdl2 libsdl2-2.0-0 libsdl2-dev libsdl2-gfx-1.0-0 libsdl2-gfx-dev libsdl2-image-2.0-0 libsdl2-mixer-2.0-0 libsdl2-net-2.0-0 -y 2>&1 | tee -a "$LOGFILE"
+
+log "Instalando pacotes adicionais e Wine 32-bit"
+sudo apt install gvfs:i386 wine32-preloader:i386 wine64-preloader wine-binfmt gstreamer1.0-libav:i386 gstreamer1.0-plugins-bad:i386 gstreamer1.0-plugins-ugly winetricks gstreamer1.0-alsa gamemode gamescope timidity opus-tools:i386 libgl1-mesa-glx:i386 -y 2>&1 | tee -a "$LOGFILE"
+
+log "Instalando vkBasalt (filtro de pós-processamento Vulkan)"
+sudo apt install meson ninja-build -y 2>&1 | tee -a "$LOGFILE"
+git clone https://github.com/DadSchoorse/vkBasalt.git && cd vkBasalt && meson --buildtype=release builddir 2>&1 | tee -a "$LOGFILE"
+ninja -C builddir install 2>&1 | tee -a "$LOGFILE"
+
+log "Configurando vkBasalt"
+mkdir -p ~/.config/vkBasalt && cp /usr/share/vkBasalt/vkBasalt.conf.example ~/.config/vkBasalt/vkBasalt.conf 2>&1 | tee -a "$LOGFILE"
+
+log "Instalando plataformas de jogos (Steam, Lutris, ScummVM, DOSBox)"
+sudo apt install steam lutris scummvm dosbox -y 2>&1 | tee -a "$LOGFILE"
+
+log "Configuração finalizada!"
